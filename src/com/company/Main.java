d638ca0772e3840469b57552a27d327a0c612d62
foxofthenorth <33484793+foxofthenorth@users.noreply.github.com>
2017-11-08 13:58:06 +0200

    first created

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import com.company.InOut;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int Loop=1;
+        while (Loop>0){
+            Loop=InOut.Cikle();
+        }
+    }
+}
